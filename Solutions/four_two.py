38a46c142a7ba5c8c5c9791e4bac63411e642900
Lewis <lewis.frampton@gmail.com>
2023-12-06 19:07:06 +0000

    line reduction for the sake of it

--- Solutions/four_two.py
+++ Solutions/four_two.py
@@ -6,21 +6,16 @@ from utils import get_input, num_to_eng, num_map, inverse_num_map
 
 def run():
     input = get_input('4')
-    copies = [1 for i in range(len(input))]
+    copies = [1 for _ in range(len(input))]
     for i, line in enumerate(input):
-        score = scoreCard(line)
-        counter = i + 1
-        while score > 0 and counter < len(copies):
-            copies[counter] += copies[i]
-            score -= 1
-            counter += 1
+        for j in range(1, scoreCard(line) + 1):
+            copies[i + j] += copies[i]
+            if i + j >= len(copies):
+                break
     return sum(copies)
 
 def scoreCard(line):
     [winners, numbers] = line.split("|")
     winners = [int(n) for n in winners.strip().split(" ")[2:] if n.isnumeric()]
     numbers = [int(n) for n in numbers.split(" ") if n.isnumeric()]
-    wins = sum(n in winners for n in numbers)
-    if wins == 0:
-        return 0
-    return wins
+    return sum(n in winners for n in numbers)
